68df1e6a3838b0ada34ab9fcb03a66f5ec446e41
SVS <victorviccc1@gmail.com>
2023-11-13 15:51:57 +0200

    Prepared OneSignal SDK

---
+++ app/src/main/java/com/severin/onesignalpushdemo/ApplicationCustom.kt
@@ -0,0 +1,30 @@
+package com.severin.onesignalpushdemo
+
+import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+import com.onesignal.OneSignal
+import com.onesignal.debug.LogLevel
+
+// NOTE: Replace the below with your own ONESIGNAL_APP_ID
+const val ONESIGNAL_APP_ID = "b29c3453-515e-4975-8966-a95a147445b0"
+
+class ApplicationCustom : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Verbose Logging set to help debug issues, remove before releasing your app.
+        OneSignal.Debug.logLevel = LogLevel.VERBOSE
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this, ONESIGNAL_APP_ID)
+
+        // requestPermission will show the native Android notification permission prompt.
+        // NOTE: It's recommended to use a OneSignal In-App Message to prompt instead.
+        CoroutineScope(Dispatchers.IO).launch {
+            OneSignal.Notifications.requestPermission(true)
+        }
+    }
+}
